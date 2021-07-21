1d0ab528b22bdb43143b35dcbc6b2be029cb0267
hy <751020872@qq.com>
2021-07-22 00:26:26 +0800

    111

--- leaf-server/src/main/java/com/sankuai/inf/leaf/server/service/SegmentService.java
+++ leaf-server/src/main/java/com/sankuai/inf/leaf/server/service/SegmentService.java
@@ -1,5 +1,7 @@
 package com.sankuai.inf.leaf.server.service;
 
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.http.HttpUtil;
 import com.alibaba.druid.pool.DruidDataSource;
 import com.sankuai.inf.leaf.IDGen;
 import com.sankuai.inf.leaf.common.PropertyFactory;
@@ -15,7 +17,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.sql.SQLException;
-import java.util.Properties;
+import java.util.*;
+import java.util.concurrent.CountDownLatch;
 
 @Service("SegmentService")
 public class SegmentService {
@@ -33,6 +36,7 @@ public class SegmentService {
             dataSource.setUrl(properties.getProperty(Constants.LEAF_JDBC_URL));
             dataSource.setUsername(properties.getProperty(Constants.LEAF_JDBC_USERNAME));
             dataSource.setPassword(properties.getProperty(Constants.LEAF_JDBC_PASSWORD));
+            dataSource.setDriverClassName(properties.getProperty(Constants.LEAF_DRIVER_CLASS_NAME));
             dataSource.init();
 
             // Config Dao
@@ -61,5 +65,55 @@ public class SegmentService {
             return (SegmentIDGenImpl) idGen;
         }
         return null;
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        int forCnt = 2;
+
+        int forThreadCnt = 300;
+
+        final List<String> urlList = new ArrayList();
+
+        final Set<Long> rightIdSet = Collections.synchronizedSet(new HashSet<Long>());
+
+
+        final CountDownLatch countDownLatch = new CountDownLatch( forCnt * forThreadCnt);
+
+        urlList.add("http://localhost:8001/api/segment/get/leaf-segment-test");
+        urlList.add("http://localhost:8002/api/segment/get/leaf-segment-test");
+        /*urlList.add("http://localhost:8003/api/segment/get/leaf-segment-test");
+        urlList.add("http://localhost:8004/api/segment/get/leaf-segment-test");
+        urlList.add("http://localhost:8005/api/segment/get/leaf-segment-test");*/
+
+
+        for(int i = 0 ; i < forCnt ; i++){
+            final int rNum = RandomUtil.randomInt(0,urlList.size());
+
+            for(int j = 0 ; j<forThreadCnt ; j++){
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try{
+                            String rStr = HttpUtil.get(urlList.get(rNum));
+
+
+                            System.out.println("id===>" + rStr);
+                                rightIdSet.add(Long.parseLong(rStr));
+
+                        }finally {
+                            countDownLatch.countDown();
+                        }
+                    }
+                }).start();
+            }
+
+        }
+
+        countDownLatch.await();
+
+        System.out.println("===rightIdSet===" + rightIdSet.size());
+
     }
 }
